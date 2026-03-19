42923c723f34bf3bec6c9c31429fd88eb0281c86
Evan Neumann <evan.neu@icloud.com>
2026-03-19 23:29:21 +0000

    envelope class with hann calculation

--- src/GranularEngine/GrainEnvelope.cpp
+++ src/GranularEngine/GrainEnvelope.cpp
@@ -17,6 +17,16 @@ void GrainEnvelope::configure(EnvelopeType type,int totalSamples)
     this->totalSamples;
 }
 
+float GrainEnvelope::calculate(float phase)
+{
+    switch (type)
+    {
+        case EnvelopeType::Hann: return getHannEnvelope(phase);
+        case EnvelopeType::Gaussian: return getGaussianEnvelope(phase);
+        case EnvelopeType::Trapezoid: return getTrapezoidEnvelope(phase);
+    }
+}
+
 float GrainEnvelope::getHannEnvelope(float samplePhase)
 {
     float HannReturnAmplitude;
